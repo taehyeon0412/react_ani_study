26f1c9a2c8d525708068d109653263377c524595
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-04-25 21:49:39 +0900

    Gestures part Two
    dragConstraints={biggerBoxRef}
    //드래그 제약
    
    dragSnapToOrigin
    //드래그 끝에는 중앙으로 가게 함
    
    dragElastic={1}
    //드래그 탄력성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { type } from "os";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,11 +10,19 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+`;
+
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background-color: white;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -32,15 +40,21 @@ const boxVar = {
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVar}
-        whileHover="hover"
-        whileTap="click"
-        whileDrag="drag"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef} //드래그 제약
+          dragSnapToOrigin //드래그 끝에는 중앙으로 가게함
+          dragElastic={1} //드래그 탄력성
+          variants={boxVar}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
@@ -57,3 +71,9 @@ delayChildren: 0.8, 자식들의 모든 딜레이
 staggerChildren: 0.2, 자식들 순서대로 딜레이 ex)첫번째 0.3 두번째 0.3*2 세번째 0.3*3... */
 
 /* 색깔은 string이 아닌 숫자 rgba로 넣어줘야 애니메이션이 작동한다 */
+
+/* ref = 코드로 특정 Element를 잡을 수있는 방법 
+기본적으로 ref 개체는 null 값으로 초기화됩니다.
+즉, 처음에는 아무것도 가리키지 않지만 요소의 ref 특성을 사용하여 
+div 요소를 가리키도록 업데이트할 수 있습니다.
+*/
