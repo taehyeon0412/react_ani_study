efca5c3fe4682a25a2d5e53feecb512ba6742aa9
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-04-25 19:31:45 +0900

    Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { type } from "os";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,7 +10,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -16,12 +18,23 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", bounce: 8 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
 
 export default App;
+
+//모션 애니메이션을 쓰려면 <motion.태그>를 쓴다.
+/* 모션 애니메이션을 적용한 스타일 컴포넌트 쓰는법 
+   => 기존 styled.div ==> 변경 styled(motion.div)*/
+
+/* variants,initial,animate */
