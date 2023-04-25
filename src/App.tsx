b1e37d3cca5340e7934a5ac4fd3a4ce651b743e1
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-04-25 20:48:36 +0900

    Variants part Two

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,56 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", bounce: 8 } },
+const Circle = styled(motion.div)`
+  place-self: center;
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVar = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.8,
+      bounce: 0.5,
+      delayChildren: 0.8, //자식들의 모든 딜레이
+      staggerChildren: 0.2, //자식들 순서대로 딜레이 ex)첫번째 0.3 두번째 0.3*2 세번째 0.3*3...
+    },
+  },
+};
+
+const circleVar = {
+  start: { opacity: 0, y: 10 },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVar} initial="start" animate="end">
+        <Circle variants={circleVar} />
+        <Circle variants={circleVar} />
+        <Circle variants={circleVar} />
+        <Circle variants={circleVar} />
+      </Box>
     </Wrapper>
   );
 }
