6690a9da0ea3863fd701347c198250fd36fc2270
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-04-28 00:55:54 +0900

    Slider part One
    AnimatePresence을 이용한 슬라이드쇼(슬라이더) 기능 구현
    (다음 버튼만)

--- src/App.tsx
+++ src/App.tsx
@@ -19,36 +19,43 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVar = {
-  start: {
-    opacity: 0,
-    scale: 0,
-  },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: { opacity: 0, y: 20 },
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 0.5 } },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing((prev) => !prev);
-  };
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  /* const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1)); */
+
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVar}
-            initial="start"
-            animate="visible"
-            exit="leaving" //element가 사라질 때 어떤 animation을 발생시킬지 정함
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVar}
+              initial="invisible"
+              animate="visible"
+              exit="exit" //element가 사라질 때 어떤 animation을 발생시킬지 정함
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      {/* <button onClick={prevPlease}>이전</button> */}
+      <button onClick={nextPlease}>다음</button>
     </Wrapper>
   );
 }
