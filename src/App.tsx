ece3e25aee519b1958faf21c157bc57659085637
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-05-01 22:02:56 +0900

    Layout animation
    1.Layout animation
    =>css의 변화를 애니메이션화 해준다
    
    2.Syncing layout animations
    =>layoutId에 같은 id를 주는 이유 : 다른 컴포넌트지만 framer한테는
      같은 prop라고 알려주기위함

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   overflow: hidden;
   background: linear-gradient(135deg, rgb(35, 75, 107), rgb(36, 108, 150));
@@ -14,64 +14,42 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
   top: 100px;
-  display: flex;
   justify-content: center;
   align-items: center;
+  display: flex;
   font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVar = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-
-  center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.5 } },
-
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.5 },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => {
+    setClicked((prev) => !prev);
   };
 
   return (
-    <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={boxVar}
-          initial="entry"
-          animate="center"
-          exit="exit" //element가 사라질 때 어떤 animation을 발생시킬지 정함
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>이전</button>
-      <button onClick={nextPlease}>다음</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
@@ -96,3 +74,7 @@ div 요소를 가리키도록 업데이트할 수 있습니다.
 */
 
 /* AnimatePresence => 자식에게 나타나거나 사라지는게 있다면 그것을 애니메이트하게 해준다*/
+/* layout => css의 변화를 애니메이션화 해준다 */
+
+/*layoutId에 같은 id를 주는 이유 : 다른 컴포넌트지만 framer한테는
+  같은 prop라고 알려주기위함 */
